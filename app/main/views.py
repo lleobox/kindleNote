1fb0b59137428b262fc69bfa4168d31bdefae1a7
lleohao <lleohao@hotmail.com>
2015-08-02 21:45:51 +0800

    add showMark table
    完成标注页面

--- app/main/views.py
+++ app/main/views.py
@@ -4,14 +4,6 @@ from . import main
 from .. import db
 from .. import modles
 
-# @app.route('/')
-# def showMark():
-#     # todo: 完成数据库列表的导出
-#     db = get_db()
-#     cur = db.execute('select id,name,content from book order by id')
-#     entries = cur.fetchall()
-#     return render_template('showMark.html', items=entries)
-
 @main.route('/')
 def index():
     return render_template('showMark.html')
--- config.py
+++ config.py
@@ -8,16 +8,20 @@ class Config:
     def init_app(app):
         pass
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev-db.sqlite')
 
+
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+
 
 class TestingConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db-back.sqlite')
+
 
 config = {
     'development': DevelopmentConfig,
