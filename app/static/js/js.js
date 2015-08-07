baddd9c3e78ff345d59072201dd888d179a0060b
lleohao <lleohao@hotmail.com>
2015-08-07 23:07:21 +0800

    8-7
    添加页面的交互样式

--- app/static/js/js.js
+++ app/static/js/js.js
@@ -1,4 +1,46 @@
-/**
- * Created by lleoh_iipevel on 2015/8/4.
- */
+containtList = $('.containt');
+deleteBtnList = $('.delete-btn');
+
+containtList.click(function(){
+    var text = $.trim(this.innerText);
+    var d = dialog({
+        title: '显示详细信息',
+        content: text,
+        width: 800,
+        height: 460,
+        quickClose: true,
+        cancelValue: '关闭',
+        cancel: function(){
+            this.close();
+        },
+    });
+    d.show();
+});
+
+deleteBtnList.click(function(){
+    var uid = this.getAttribute('uid');
+    var url = '/delete='+uid;
+    var d = dialog({
+        title: '是否删除这条内容',
+        content: ' ',
+        okValue: '确定',
+        ok: function(){
+            $.get(url,function(){
+                dialog({
+                    content: '删除成功',
+                    quickClose: true,
+                    onclose: function(){
+                        window.location.href = '/';
+                    }
+                }).show();
+            })
+        },
+        cancelValue: '取消',
+        cancel: function(){
+            this.close();
+        },
+    });
+    d.show();
+});
+
 
