beeeb9dbe2f79c75d4e11eeb9120360f8c44fdb4
lleohao <lleohao@hotmail.com>
2015-09-16 19:54:05 +0800

    设置cookie过期期限 防止每次访问是都需要重置数据库

--- app/init/views.py
+++ app/init/views.py
@@ -1,14 +1,18 @@
 from flask import render_template, redirect, url_for, request, make_response, session
+from datetime import timedelta
 
 from . import init
 from . import init_db
 from .. import db
 from .. import modles
 
-@init.before_app_first_request
-def first_init():
-    init_db.init_db()
 
 @init.route('/')
 def loding():
-    return redirect(url_for('main.selectBook'))
+    if request.cookies.get('expir'):
+        return redirect('selectBook')
+    else:
+        init_db.init_db()
+        resp = make_response(redirect('selectBook'))
+        resp.set_cookie('expir', 'true', max_age=1800)
+        return resp
